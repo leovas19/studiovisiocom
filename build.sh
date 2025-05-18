f57a01254546d6b0accccb649147a6cd6a337188
leovas19 <leovasseur30@gmail.com>
2025-05-18 20:29:51 +0200

    tout est ok

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-# build.sh
+set -e  # Arrêter à la première erreur
 
+echo "✅ INSTALLATION DES DÉPENDANCES"
 pip install -r requirements.txt
+
+echo "📦 COLLECTE DES FICHIERS STATIQUES"
 python manage.py collectstatic --noinput
+
+echo "🧩 MIGRATIONS BASE DE DONNÉES"
 python manage.py migrate
+
+echo "✅ BUILD TERMINÉ AVEC SUCCÈS"
+
