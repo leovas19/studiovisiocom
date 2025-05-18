3ff16f2f74861d055807933675b71a1a78418bbc
leovas19 <leovasseur30@gmail.com>
2025-05-18 20:57:15 +0200

    Mail

--- Exemple/settings.py
+++ Exemple/settings.py
@@ -124,13 +124,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'studiovisiocom@gmail.com'
-EMAIL_HOST_PASSWORD = 'ta_clé_générée_Gmail_ici'
+EMAIL_HOST_PASSWORD = 'tgslakbqogqwrydp'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CSRF_TRUSTED_ORIGINS = ['https://studiovisiocom-production.up.railway.app']
 
