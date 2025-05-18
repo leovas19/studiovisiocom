04f2bf6ea16a7a0279b413b49fe036679aa1ce3e
leovas19 <leovasseur30@gmail.com>
2025-05-18 18:53:40 +0200

    ✅ Ajout de l'URL Railway à ALLOWED_HOSTS

--- Exemple/settings.py
+++ Exemple/settings.py
@@ -18,7 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['studiovisiocom-production.up.railway.app']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
